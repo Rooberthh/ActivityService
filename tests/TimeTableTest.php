0d425b2131a6a123519253247e6bf9add9f020c3
rooberthh <roberth.evaldsson@hotmail.com>
2019-09-30 11:42:16 +0200

    timetables can be fetched

--- tests/TimeTableTest.php
+++ tests/TimeTableTest.php
@@ -1,6 +1,5 @@
 <?php
 
-    use Carbon\Carbon;
     use Laravel\Lumen\Testing\DatabaseMigrations;
 
     class TimeTableTest extends TestCase
@@ -25,5 +24,16 @@
 
             $response->assertResponseStatus(201);
             $this->seeInDatabase('timetables', $timetable->toArray());
+        }
+
+        /** @test */
+        function a_user_can_fetch_timetables()
+        {
+            $activities = create('App\Timetable', [], 10);
+
+            $response = $this->json('get', 'api/timetable');
+
+            $response->assertResponseStatus(200);
+            $response->seeJsonEquals($activities->toArray());
         }
     }
